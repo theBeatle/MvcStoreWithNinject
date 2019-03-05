2684103abad53216aa0a081d9407a178e848d872
DESKTOP-EPN9CP6\thebe <oleh.knyaz@gmail.com>
2019-03-05 21:05:43 +0200

    workable project

--- Store/Controllers/HomeController.cs
+++ Store/Controllers/HomeController.cs
@@ -19,26 +19,28 @@ namespace Store.Controllers
 
         public ActionResult About()
         {
-            var result = bll.GetCountriesByConcreteMark("sddd");
-
-
             return View();
         }
 
         [HttpPost]
         public ActionResult About(AboutModel model)
         {
-            var result = bll.GetCountriesByConcreteMark(model.ConcreteMark);
-
-
-            return View();
+            var result = bll.GetProducersCountByCocncreteMark(model.ConcreteMark);
+            model.Quantity = result;
+            return View(model);
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactModel model)
+        {
+            model.Countries = bll.GetCountriesByConcreteMark(model.ConcreteMark);
+            return View(model);
         }
     }
 }
---
+++ Store/Models/ContactModel.cs
@@ -0,0 +1,12 @@
+﻿using BLL;
+using System.Collections.Generic;
+
+namespace Store.Models
+{
+    public class ContactModel
+    {
+        public string ConcreteMark { get; set; }
+        public IEnumerable<CountryBll> Countries { get; set; }
+    }
+
+}
